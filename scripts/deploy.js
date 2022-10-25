e100ceb253028dbf6c49e282a436357e0ccdd282
Mel <angel.melendez.c@gmail.com>
2022-10-25 08:05:43 -0500

    Create PokemonWorld smart contract and Pikachu NFT metadata (json), configure hardhat and run scripts/deploy.js to deploy to Mumbai network and mint 1st NFT (Pikachu)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const PokemonWorld = await ethers.getContractFactory('PokemonWorld');
+  const pokemonWorld = await PokemonWorld.deploy('PokemonWorld', 'PKMN');
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await pokemonWorld.deployed();
+  console.log('Success! Contract was deployed to: ', pokemonWorld.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  await pokemonWorld.mint(
+    'https://ipfs.io/ipfs/Qmc3EXicSsEjoE2N4QkZJSWRie7fQBBZwh4R5chBPRr9BH'
   );
+
+  console.log('NFT successfully minted!');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
